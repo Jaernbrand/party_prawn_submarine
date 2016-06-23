ceaa83620d92cc7511391edbae31cb6260e9d9ba
Jaernbrand <henrikjarnbrand@gmail.com>
2016-06-23 18:17:26 +0200

    Create English

---
+++ party_prawn_submarine/messages/english.rb
@@ -0,0 +1,17 @@
+
+# Responsible for creating the dictionary of user messages in english.
+module English
+
+	# Returns a Hash with all user messages in english.
+	#
+	# * *Returns* :
+	#   - Hash containing the user messages in english
+	# * *Return* *Type* :
+	#   - Hash<Symbol, String>
+	def self.messages
+		{:winner => "The winner is #{}!",
+		:no_winner => "No one won"}
+	end
+
+end
+
