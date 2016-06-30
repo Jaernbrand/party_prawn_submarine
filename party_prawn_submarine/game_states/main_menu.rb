81760b1171f4328a211f187845d8e95c4c181d37
Jaernbrand <henrikjarnbrand@gmail.com>
2016-06-30 21:55:57 +0200

    Create main menu

---
+++ party_prawn_submarine/game_states/main_menu.rb
@@ -0,0 +1,17 @@
+
+# The main menu of the game.
+class MainMenu
+
+	# The width of the MainMenu
+	attr_accessor :width
+	
+	# The height of the MainMenu
+	attr_accessor :height
+
+	# The GameWindow that the current MainMenu is associated with
+	attr_accessor :window
+
+	def initialize
+	end
+
+end
---
+++ tests/game_states/test_main_menu.rb
@@ -0,0 +1,17 @@
+
+require 'test/unit'
+require 'minitest/mock'
+
+require 'game_states/main_menu'
+
+class MainMenuTester < Test::Unit::TestCase
+
+	def setup
+		@menu = MainMenu.new
+	end
+
+	def test
+		assert false, "Not implemented"
+	end
+
+end
